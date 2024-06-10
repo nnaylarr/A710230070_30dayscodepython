712c82e8e536a94ea9c30e6713cd14726eb9470c
nnaylarr <a710230070@student.ums.ac.id>
2024-06-10 20:34:36 +0700

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,17 +1,4 @@
-def main():
-    print("Program Kalkulator Sederhana")
-    try:
-        num1 = float(input("Masukkan angka pertama: "))
-        num2 = float(input("Masukkan angka kedua: "))
-        
-        print(f"Hasil penjumlahan: {num1 + num2}")
-        print(f"Hasil pengurangan: {num1 - num2}")
-        print(f"Hasil perkalian: {num1 * num2}")
-        print(f"Hasil pembagian: {num1 / num2}")
-    except ValueError:
-        print("Input tidak valid. Harap masukkan angka.")
-    except ZeroDivisionError:
-        print("Pembagian dengan nol tidak diperbolehkan.")
-    
-if __name__ == "__main__":
-    main()
+import math
+def hello_nayla():
+    print("Hello Nayla")
+hello_nayla()
